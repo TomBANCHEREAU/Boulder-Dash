be706e95ad71da18e8a4ed425ad3c40c0fecf2f5
TomBANCHEREAU <banchereau.tom@gmail.com>
2019-05-22 17:47:16 +0900

    ZRQEHSTYH

---
+++ BoulderDash/BoulderDash.PlayerController/src/main/java/com/TomBAN/BoulderDash/PlayerController/KeyBoardController.java
@@ -0,0 +1,28 @@
+package com.TomBAN.BoulderDash.PlayerController;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardController implements KeyListener{
+	
+	
+	public KeyBoardController(int keyUp,int keyLeft,int keyDown,int keyRight) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//e.getkey
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
