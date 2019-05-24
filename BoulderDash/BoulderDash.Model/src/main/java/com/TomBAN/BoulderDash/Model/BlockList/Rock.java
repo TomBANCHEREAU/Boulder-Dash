5615cc3c1a954e8ec3b735dde35764dbf5fb7ddc
TomBANCHEREAU <banchereau.tom@gmail.com>
2019-05-24 19:22:13 +0900

    Fixing Rock animation

--- BoulderDash/BoulderDash.Model/src/main/java/com/TomBAN/BoulderDash/Model/BlockList/Rock.java
+++ BoulderDash/BoulderDash.Model/src/main/java/com/TomBAN/BoulderDash/Model/BlockList/Rock.java
@@ -3,14 +3,24 @@ package com.TomBAN.BoulderDash.Model.BlockList;
 import com.TomBAN.BoulderDash.Model.Instable;
 import com.TomBAN.BoulderDash.Model.MovableBlock;
 import com.TomBAN.BoulderDash.Model.Pushable;
+import com.TomBAN.BoulderDash.Model.StrategyList.AnimationStrategy;
 import com.TomBAN.BoulderDash.Model.StrategyList.RockStrategy;
 
-public class Rock extends MovableBlock implements Instable,Pushable{
-	private static final String[] IMAGE = {"Rock/ROCK_0.png","Rock/ROCK_1.png","Rock/ROCK_2.png","Rock/ROCK_3.png"};
+public class Rock extends MovableBlock implements Instable, Pushable {
+	private static final String[] IMAGE = { "Rock/ROCK_0.png", "Rock/ROCK_1.png", "Rock/ROCK_2.png",
+			"Rock/ROCK_3.png" };
+
 	public Rock(int x, int y) {
 		super(IMAGE, x, y);
 		setStrategy(new RockStrategy(false));
 	}
-	
-	
+
+	@Override
+	public int getImageId() {
+		if (getStrategy() instanceof AnimationStrategy) {
+			return (getLastUpdate() / 2) % IMAGE.length;
+		}
+		return 0;
+	}
+
 }
